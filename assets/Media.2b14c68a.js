a89f21856249a359c7411febbe620e5f85632e0a
azertypow <azertypow@users.noreply.github.com>
2022-10-19 13:04:51 +0000

    deploy: 49a924e9cd12f458a401970511e600846d6d84bb

---
+++ assets/Media.2b14c68a.js
@@ -0,0 +1,1 @@
+import{d as e,_ as t,o,c as s}from"./index.51cbcfce.js";const c=e({computed:{sortedArticle(){}}});const n={class:"v-media"};function _(a,d,r,i,p,l){return o(),s("div",n)}const m=t(c,[["render",_]]);export{m as default};
