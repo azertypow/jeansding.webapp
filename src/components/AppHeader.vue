49a924e9cd12f458a401970511e600846d6d84bb
azertypow <azertypow@gmail.com>
2022-10-19 15:04:21 +0200

    fix: correction

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,7 +3,7 @@
 
     <div class="v-app-header__title v-app-header__title">
       <router-link to="/"       class="v-app-header__title__left jd-font-xxl">Inventory</router-link>
-      <div                      class="v-app-header__title__center jd-font-xxl">Jeansding</div>
+      <div                      class="v-app-header__title__center jd-font-xxl">Jeansdinge</div>
       <router-link to="/media"  class="v-app-header__title__right jd-font-xxl">Media</router-link>
       <div class="v-app-header__ui"></div>
     </div>
