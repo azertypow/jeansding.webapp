b04d265238ceb4e3609369d55376e065f1b311c3
azertypow <azertypow@gmail.com>
2022-10-20 18:32:12 +0200

    fix: close onpen inventory when page change

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Inventory from '../views/Inventory.vue'
+import {stateStore} from "../stores/stateStore"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,6 +24,8 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
 
   document.querySelector('html')!.className = `is-${to.name?.toString() || 'null'}`
+
+  stateStore().currentOpenObject = null
 
   next()
 })
