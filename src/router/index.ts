93198827a7e292020b5c0215afe1a8979f386029
azertypow <azertypow@gmail.com>
2022-11-13 14:35:24 +0100

    feat: hash history mode

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Inventory from '../views/Inventory.vue'
 import {stateStore} from "../stores/stateStore"
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
