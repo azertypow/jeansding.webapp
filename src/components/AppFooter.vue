69e3838462d9a22f3c28e0f1c3425075dbd4509a
azertypow <azertypow@gmail.com>
2023-06-02 10:17:45 +0200

    fix: build error

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -111,7 +111,7 @@
                 @click="togglePlayerState"
         >
             <svg
-                    v-if="this.playerState === 'is-paused'"
+                    v-if="playerState === 'is-paused'"
                     class="v-app-footer__player"
                     width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"
             >
